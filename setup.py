bdcfe7a74bd246d8b6745f090453998ef97004b2
Linjian <Leoyang158@tamu.edu>
2022-07-05 09:16:22 -0700

    done

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='testboxTryOut',
     version='0.0.3',
     author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
+    author_email='leoyang158@gmail.com',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mike-huls/toolbox',
+    url='https://github.com/Leoyang158/pip_library',
     project_urls = {
-        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
     },
     license='MIT',
-    packages=['toolbox'],
+    packages=['testboxTryOut'],
     install_requires=['requests'],
 )
